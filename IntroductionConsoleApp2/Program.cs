1c68e9a447265691e40f4b3ee5ee7a2f5a1b37cd
HP <HP@DESKTOP-0T28H3U>
2023-10-29 01:00:34 +0600

    Add project files.

---
+++ IntroductionConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionConsoleApp2
+{
+    class Program
+    {
+        static void Main1()
+        {
+            Console.WriteLine("Please Enter Your Name....");
+            string username = Console.ReadLine();
+            //Console.WriteLine("Hi " + username);
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Main1();
+            Console.ReadLine();
+            
+
+        }
+
+    }
+}
